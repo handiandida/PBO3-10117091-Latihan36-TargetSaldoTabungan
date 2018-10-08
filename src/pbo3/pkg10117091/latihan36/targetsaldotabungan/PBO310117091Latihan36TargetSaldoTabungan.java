2289d4f99728d4cd2c5e2fb2497b70580159135c
Dida Handian <handiandida@gmail.com>
2018-10-08 22:27:13 +0700

    TargetSaldoTabungan

---
+++ src/pbo3/pkg10117091/latihan36/targetsaldotabungan/PBO310117091Latihan36TargetSaldoTabungan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117091.latihan36.targetsaldotabungan;
+
+/* @author
+* NAMA  : DIDA HANDIAN
+* KELAS : PBO3
+* NIM   : 10117091
+* Deskripsi Program : 
+*/
+public class PBO310117091Latihan36TargetSaldoTabungan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Tabungan tab = new Tabungan();
+
+        tab.saldo = 3500000;
+        tab.bunga = 8;
+        int saldoTarget = 6000000;
+
+        tab.HitungLamaSaldoTarget(tab.saldo,saldoTarget);
+    }
+    
+}
